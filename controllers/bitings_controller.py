efef24acee2575138599818d81cfee8cfcc63003
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-03 13:01:31 +0100

    adds select, select_all, delete_all and save methods to biting_repository

--- controllers/bitings_controller.py
+++ controllers/bitings_controller.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
+from models.biting import Biting
+from repositories import zombie_repository, human_repository
+
 bitings_blueprint = Blueprint("bitings", __name__)
+
 
 # INDEX
 
